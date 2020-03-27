b1ffdd7b41b641a02eb8a9d86850f97010f2e949
SolKuczala <sol.kuczala@gmail.com>
2020-03-27 12:20:31 +0100

    update files / test server

--- project/server.go
+++ project/server.go
@@ -7,11 +7,10 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		print(c)
 		c.JSON(400, gin.H{
 			"message": "pong",
 		})
-		print(c)
+		println(c) //prints this: 0xc00032e380
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
