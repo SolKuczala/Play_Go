512325bffe5acfbd123c93dc04b7f0da06116765
SolKuczala <sol.kuczala@gmail.com>
2020-03-30 21:11:17 +0200

    update files / testing things

--- project/server.go
+++ project/server.go
@@ -1,16 +1,38 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(400, gin.H{
-			"message": "pong",
-		})
-		println(c) //prints this: 0xc00032e380
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.GET("/create-game", createGame)
+	r.POST("/send-play", sendMove)
+
+	r.Run(":9090") // listen and serve on 0.0.0.0:8080
 }
+
+func createGame(c *gin.Context) {
+	c.JSON(200, gin.H{})
+	println(c) //prints this: 0xc00032e380
+}
+
+func sendMove(c *gin.Context) {
+	//tengo que recibir
+	b := make([]byte, 2048)
+	r, _ := c.Request.Body.Read(b)
+	fmt.Printf("Me llego:\n%+v", r)
+}
+
+/*proye:
+que dos maquinas jueguen al tic tac toe.
+una maquina va a crear el tablero
+otra es la que pide y empieza el juego
+el p1 envia el codigo donde quiere que se marque su jugada
+el p2 guarda eso y responde con la jugada siguiente
+-----------
+dos dockers que al levantarlos hacen la jugada
+un externo cuenta los resultados, puede sacar estadisticas
+*/
---
+++ project/structs.go
@@ -0,0 +1,23 @@
+package main
+
+type Board struct {
+	Place [4]string `json:"campo1"`
+}
+
+/*
+"line 1": []string{"", "", ""},
+"line 2": []string{"", "", ""},
+"line 3": []string{"", "", ""},
+*/
+
+// ["x","o"]
+//lenght := 5
+//arrayDeStrings := make([]string, lenght)
+
+// [["x","o"], ["x","o"], ["x","o"]]
+//matrix := make([][]string, largo)
+// [[],[],[],[],[]]
+/*for _, var := range arrayDeArrays {
+	arrayDeArrays[i] = make([]string, lenght)
+}
+*/
